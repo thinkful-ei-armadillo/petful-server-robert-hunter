2491c4c545a6a187fa4e5bbcffcf4ed92f49d95c
bobnearents <bobnearents@yahoo.com>
2019-04-12 12:32:50 -0700

    made some server

--- Petful-server/src/cat/cat-service.js
+++ Petful-server/src/cat/cat-service.js
@@ -1,19 +1,9 @@
 'use strict';
+const cats = require('./cats.json');
 
 const CatService = {
   getCat() {
-    let cat = {
-      imageURL:
-        'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-      imageDescription:
-        'Orange bengal cat with black stripes lounging on concrete.',
-      name: 'Fluffy',
-      sex: 'Female',
-      age: 2,
-      breed: 'Bengal',
-      story: 'Thrown on the street'
-    };
-    return cat;
+    return cats;
   }
 };
 
--- Petful-server/src/dog/dog-service.js
+++ Petful-server/src/dog/dog-service.js
@@ -1,19 +1,8 @@
 'use strict';
-
+const dogs = require('./dogs.json');
 const DogService = {
   getDog() {
-    let dog = {
-      imageURL:
-        'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-      imageDescription:
-        'A smiling golden-brown golden retreiver listening to music.',
-      name: 'Zeus',
-      sex: 'Male',
-      age: 3,
-      breed: 'Golden Retriever',
-      story: 'Owner Passed away'
-    };
-    return dog;
+    return dogs;
   }
 };
 
