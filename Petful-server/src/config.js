6527161fd82f9d0086f8415fb8bb6ca0f4e8cfd9
bobnearents <bobnearents@yahoo.com>
2019-04-12 14:48:23 -0700

    fixed deployment stuff

--- Petful-server/src/config.js
+++ Petful-server/src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT || 8080,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
 }
