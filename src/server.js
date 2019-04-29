64be3933ccacdc8200953def6200cfde95ba5388
Hunter Kreshock <hkreshock@gmail.com>
2019-04-29 12:13:50 -0700

    port

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ app.use(function(req, res, next) {
 // Catch-all Error handler
 // Add NODE_ENV check to prevent stacktrace leak
 app.use(function(err, req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', PORT);
   res.status(err.status || 500);
   res.json({
     message: err.message,
@@ -31,8 +32,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-// const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log('Serving on PORT');
 });
