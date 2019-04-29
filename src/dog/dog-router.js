2a7053a5362736dde1cdbd2f0d5e54c8a0079d24
Hunter Kreshock <hkreshock@gmail.com>
2019-04-29 12:25:11 -0700

    port

--- src/dog/dog-router.js
+++ src/dog/dog-router.js
@@ -6,7 +6,7 @@ const express = require('express');
 const DogRouter = express.Router();
 
 DogRouter.route('/').get((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', { mode: 'no-cors' });
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.json(DogService.getDog());
 });
 
